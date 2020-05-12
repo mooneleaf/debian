39eb49f1d32aeb2386cd011506cae0e15497950d
Stephen Reay <stephen@koalephant.com>
2020-05-12 15:33:55 +0700

    Improve hypervisor specific tool installations

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -2,6 +2,24 @@
 
 SSH_USER="${SSH_USERNAME:-vagrant}"
 
+install_from_iso() {
+	local iso="$1"
+
+	apt-get install -y "linux-headers-$(uname -r)" build-essential perl dkms
+	mkdir -p /mnt/tools
+	mount -o loop,ro "$iso" /mnt/tools
+	retCode=0
+
+	sh /mnt/tools/VBoxLinuxAdditions.run --nox11 || retCode=$?
+
+	if [ ${retCode} -eq 1 ]; then
+		printf -- 'VirtualBox Guest Additions installation failed\n' >&2
+		exit 1
+	fi
+	umount /mnt/tools
+	rmdir /mnt/tools
+}
+
 if [ "${PACKER_BUILDER_TYPE}" = 'virtualbox-iso' ]; then
 	case "$(printf -- '%s' "${GUEST_TOOLS:-}" | tr '[:upper:]' '[:lower:]')" in
 		true|yes|on|1)
@@ -16,31 +34,27 @@ if [ "${PACKER_BUILDER_TYPE}" = 'virtualbox-iso' ]; then
 	esac
 
 	case "$(printf -- '%s' "${GUEST_TOOLS_DISTRO:-}" | tr '[:upper:]' '[:lower:]')" in
-		true|yes|on|1)
-			printf -- 'Package: virtualbox-guest-utils virtualbox-guest-dkms\nPin: release a=%s\nPin-Priority: 500\n\n' "$(lsb_release -sc)-updates" "$(lsb_release -sc)-backports" > /etc/apt/preferences.d/virtualbox-additions
-			apt-get -y install virtualbox-guest-utils virtualbox-guest-dkms
-		;;
+		(*virtualbox*)
+			printf -- 'Package: virtualbox-guest-utils virtualbox-guest-dkms virtualbox-guest-additions-iso\nPin: release a=%s\nPin-Priority: 500\n\n' "$(lsb_release -sc)-updates" "$(lsb_release -sc)-backports" > /etc/apt/preferences.d/virtualbox-additions
 
-		*)
-			apt-get install -y linux-headers-$(uname -r) build-essential perl dkms
-			mkdir -p /mnt/tools
-			if [ -f "/home/${SSH_USER}/tools-manual/VBoxGuestAdditions.iso" ]; then
-				mount -o loop "/home/${SSH_USER}/tools-manual/VBoxGuestAdditions.iso" /mnt/tools
-			else
-				mount -o loop "/home/${SSH_USER}/VBoxGuestAdditions.iso" /mnt/tools
+			if apt-cache policy virtualbox-guest-utils | grep 'Candidate: [[:digit:]]' > /dev/null; then
+				apt-get -y install virtualbox-guest-utils virtualbox-guest-dkms
+			elif apt-cache policy virtualbox-guest-additions-iso | grep 'Candidate: [[:digit:]]' > /dev/null; then
+				apt-get -y install virtualbox-guest-additions-iso
+				install_from_iso "/usr/share/virtualbox/VBoxGuestAdditions.iso"
+				apt-get -y purge virtualbox-guest-additions-iso
 			fi
-
-			retCode=0
-			sh /mnt/tools/VBoxLinuxAdditions.run --nox11 || retCode=$?
-			if [ ${retCode} -eq 1 ]; then
-				printf -- 'VirtualBox Guest Additions installation failed\n' >&2
-				exit 1
-			fi
-			umount /mnt/tools
-			rmdir /mnt/tools
-
 		;;
 	esac
+
+
+	if ! command -v VBoxControl > /dev/null; then
+		if [ -f "/home/${SSH_USER}/tools-manual/VBoxGuestAdditions.iso" ]; then
+			install_from_iso "/home/${SSH_USER}/tools-manual/VBoxGuestAdditions.iso"
+		else
+			install_from_iso "/home/${SSH_USER}/VBoxGuestAdditions.iso"
+		fi
+	fi
 
 	printf -- '- VirtualBox Guest Additions version %s\n' "$(VBoxControl -v)" > /tmp/guest-additions-version.txt
 	rm -frv "/home/${SSH_USER}/VBoxGuestAdditions.iso" /home/${SSH_USER}/tools-manual/ "/home/${SSH_USER}/.vbox_version"
